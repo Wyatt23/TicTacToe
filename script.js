91459e519cac1d2a007202e89dee033395ad56cc
Adrian Wyatt <Adi.Wyatt@outlook.com>
2016-03-04 07:16:41 -0500

    replaced files that failed to merge

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
-$(document).ready(function(){
 
+firstPlayerGo=true;
+secondPlayerGo=false;
+//variable of
 
-
-
-
-
-
-
+$(".button").click(function(){
+  if (firstPlayerGo===true && secondPlayerGo===false){
+    // $(this).text("X");
+    $(this).addClass("xclaimed")
+    firstPlayerGo=false;
+    secondPlayerGo=true;
+    var X=(this.id)
+    console.log("X chose "+this.id)
+    console.log(this)
+  }
+  else if(firstPlayerGo===false && secondPlayerGo===true){
+    // $(this).text("O")
+    $(this).addClass("oclaimed")
+    firstPlayerGo=true;
+    secondPlayerGo=false;
+    var O = (this.id)
+    console.log("O chose "+this.id)
+    console.log(this)
+  }
 
 });
+
+
+
+
+
+
+//call function:
+// });
